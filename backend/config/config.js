d2005343dc6f659ff14c8d0346a2333f88cfaad2
Arham <k200195@nu.edu.pk>
2024-02-14 13:26:43 +0500

    re-editing my config.json files

--- backend/config/config.js
+++ backend/config/config.js
@@ -1,12 +1,10 @@
 module.exports = {
-    development: {
-      username: 'faris',
-      password: '12345',
-      database: 'skillsmatch',
-      host: '127.0.0.1',
-      port: 5432,
-      dialect: 'postgres', // Specify your database dialect here
-    },
-    // Add configurations for other environments (e.g., production, test) if needed
-  };
-  
+  development: {
+    username: 'postgres',
+    password: 'arham123',
+    database: 'skillsmatch',
+    host: '127.0.0.1',
+    dialect: 'postgres', // Specify your database dialect here
+  },
+  // Add configurations for other environments (e.g., production, test) if needed
+};
