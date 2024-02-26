492cd60c2b1eafdff0cf28f4711595ad2c3ee2c6
Arham <k200195@nu.edu.pk>
2024-02-26 16:32:07 +0500

    changed config

--- backend/config/config.js
+++ backend/config/config.js
@@ -2,7 +2,7 @@ module.exports = {
     development: {
       username: 'postgres',
       password: 'arham123',
-      database: 'skillsmatch3',
+      database: 'skillsmatch5',
       host: '127.0.0.1',
       dialect: 'postgres', // Specify your database dialect here
     },
