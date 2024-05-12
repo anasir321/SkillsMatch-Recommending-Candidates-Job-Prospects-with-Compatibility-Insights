58e5cb10de0a9bb03adadf9a9af11d005317011c
Arham <k200195@nu.edu.pk>
2024-05-12 11:23:26 +0500

    revamped candidate profile page, candidate resume can now be viewed

--- frontend/src/app/components/candidate-details/skills.tsx
+++ frontend/src/app/components/candidate-details/skills.tsx
@@ -1,18 +1,23 @@
 import React from "react";
 
-const Skills = () => {
+interface Props {
+  skills: string | undefined | null;
+}
+
+const Skills: React.FC<Props> = ({ skills }) => {
+  // Check if skills is a string and not undefined or null
+  if (typeof skills !== 'string' || !skills) {
+    return null; // or handle this case as per your requirements
+  }
+
+  // Use non-null assertion operator to assure TypeScript that skills is not null or undefined
+  const skillsArray = skills!.split(',').map(skill => skill.trim());
+
   return (
     <ul className="style-none skill-tags d-flex flex-wrap pb-25">
-      <li>Figma</li>
-      <li>HTML5</li>
-      <li>Illustrator</li>
-      <li>Adobe Photoshop</li>
-      <li>WordPress</li>
-      <li>jQuery</li>
-      <li>Web Design</li>
-      <li>Adobe XD</li>
-      <li>CSS</li>
-      <li className="more">3+</li>
+      {skillsArray.map((skill, index) => (
+        <li key={index}>{skill}</li>
+      ))}
     </ul>
   );
 };
